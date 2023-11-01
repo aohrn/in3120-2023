e366289227304ab5254a3debbdf8d110c3d06a7e
Oliver Ruste Jahren <oliverrj@ifi.uio.no>
2023-11-01 14:09:29 +0100

    Solution sketch for assignment D.

--- in3120/betterranker.py
+++ in3120/betterranker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import math
 from .ranker import Ranker
 from .corpus import Corpus
 from .posting import Posting
@@ -19,20 +20,38 @@ class BetterRanker(Ranker):
     See Section 7.1.4 in https://nlp.stanford.edu/IR-book/pdf/irbookonlinereading.pdf.
     """
 
+    # These values could be made configurable. Hardcode them for now.
+    _dynamic_score_weight = 1.0
+    _static_score_weight = 1.0
+    _static_score_field_name = "static_quality_score"
+    _static_score_default_value = 0.0
+
     def __init__(self, corpus: Corpus, inverted_index: InvertedIndex):
         self._score = 0.0
         self._document_id = None
         self._corpus = corpus
         self._inverted_index = inverted_index
-        self._dynamic_score_weight = 1.0
-        self._static_score_weight = 1.0
-        self._static_score_field_name = "static_quality_score"
 
     def reset(self, document_id: int) -> None:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        self._score = 0.0
+        self._document_id = document_id
 
     def update(self, term: str, multiplicity: int, posting: Posting) -> None:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        assert term is not None
+        assert multiplicity > 0
+        assert posting is not None
+        assert posting.term_frequency > 0
+        assert posting.document_id == self._document_id
+        tf_score = 1.0 + math.log10(posting.term_frequency)
+        idf_score = math.log10(self._corpus.size() / float(self._inverted_index.get_document_frequency(term)))
+        self._score += (1.0 + math.log10(multiplicity)) * tf_score * idf_score
 
     def evaluate(self) -> float:
-        raise NotImplementedError("You need to implement this as part of the assignment.")
+        # Now that the dynamic (query-dependent) score is fully updated, combine it
+        # with the static (query-independent) score using a simple weighted sum. Other
+        # ways of combining the two are plausible. In a large real-world search system,
+        # weights would be machine-learnt offline and it'd be up to the chosen ML model
+        # how to best combine the many features to yield a compound relevance score.
+        document = self._corpus[self._document_id]
+        static_quality_score = float(document[self._static_score_field_name] or self._static_score_default_value)
+        return (self._dynamic_score_weight * self._score) + (self._static_score_weight * static_quality_score)
